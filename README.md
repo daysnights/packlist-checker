cb077b570451ccbb5ae2a969d9dd8363cf749fb9
Days <days.rizon@gmail.com>
2015-09-10 21:16:41 -0700

    code style changes and updated readme

--- README.md
+++ README.md
@@ -1,27 +1,25 @@
-# packlist-checker
-Node.js app that checks for changes of a packlist. If a change is detected, it sends the packlist as a json array to an api endpoint, and authenticates with an api key. App should be run by cron, it is up to the user on how frequently they want to check. 5 Minutes is generally recommended. packlist-checker should be run by the same user as your bot.
-### Install Guide
-* Download the files from this git
-* Put in a folder called packlist-checker
-* Rename the settings.js file and edit it
-* For Ubuntu or Debian, install NPM and Node
+# packlist-checker #
+## Description ##
+Node.js app that checks for changes of a packlist. If a change is detected, it sends the packlist as a json array to an api endpoint, and authenticates with an api key. App should be run by a cron job. It is up to the user on how frequently they want to check, 5 Minutes is generally recommended. packlist-checker should be run by the same user as your bot.
+## Install Guide ##
+**Ubuntu & Debian**
 ```sh
-$ apt-get install npm node
-```
-* cd to the app folder
-* Use NPM to install dependencies
-```sh
+$ sudo apt-get install npm node
+$ git clone git://github.com/daysnights/packlist-checker.git
+$ cd packlist-checker
 $ npm install
 ```
-* Run the app manually to verify it works
+Rename the settings.js file and edit it  
+Run the app to manually to verify it works
 ```sh
 $ node main.js
 ```
-* Add a cron job
+Add a cron job
 ```sh
-$ */5 * * * * /usr/local/bin/node /absolute/path/to/main.js
+$ contab -e
+$ */5 * * * * /usr/local/bin/node /absolute/path/to/packlist-checker/main.js
 ```
-## Version
+## Version ##
 0.0.1
-## Support
+## Support ##
 Open an Issue on Github or message Days on Rizon.
--- main.js
+++ main.js
@@ -6,31 +6,31 @@ var request = require('request');
 var settings = require('./settings.js');
 var regexBotName = /MSG\s+([^\s]+)/i // bot name match
 var regexPack = /^#(\d+).*?(\d+).*?(\d+\.?\d*\w).\s(.*$)/i // pack match
-var botName = '';
+var BOTNAME = '';
 var totalPacks = 0;
 var packs = [];
 
 // settings.apiEndPoint;
 
-function log(text){
+function log(text) {
   var date = moment().utc().format('YYYYMMDDTHH:mm:ss');
   var message = date + ' : ' + text + '\n';
-  fs.appendFile(settings.logFile, message, function(err){
+  fs.appendFile(settings.logFile, message, function(err) {
     if (err) return console.log(err);
   });
 }
 
-function error(reason){
+function error(reason) {
   var date = moment().utc().format('YYYYMMDDTHH:mm:ss');
   var message = date + ' ERROR: ' + reason + '\n';
-  fs.appendFile(settings.logFile, message, function(err){
+  fs.appendFile(settings.logFile, message, function(err) {
     if (err) return console.log(err);
     process.exit();
   });
 }
 
-function getBotName(line){
-  if (line.match(regexBotName)){
+function getBotName(line) {
+  if (line.match(regexBotName)) {
     var nameResult = line.match(regexBotName);
     botName = nameResult[1];
     console.log(botName);
@@ -40,14 +40,14 @@ function getBotName(line){
   }
 }
 
-function parsePacklist(packList){
-  fs.readFile(packList, 'utf8', function(err, data){
+function parsePacklist(packList) {
+  fs.readFile(packList, 'utf8', function(err, data) {
     if (err) error(err);
     data = data.split('\n');
     // parse each line looking for packs
-    for (var i = 0; i < data.length; i++){
+    for (var i = 0; i < data.length; i++) {
       var line = data[i].match(regexPack);
-      if (line){
+      if (line) {
         totalPacks++;
         packs.push({id: line[1], gets: line[2], size: line[3], name: line[4]});
       }
@@ -56,30 +56,30 @@ function parsePacklist(packList){
   });
 }
 
-function savePacklistJson(data, packListJson){
-  fs.writeFile(packListJson, data, function(err){
+function savePacklistJson(data, packListJson) {
+  fs.writeFile(packListJson, data, function(err) {
     if (err) return error(err);
   });
 }
 
-function sendPacklist(data){
+function sendPacklist(data) {
   request({
       url: settings.apiEndPoint,
       headers: { 'api': settings.apiKey },
       method: 'POST',
       json: data
-  }, function(err, res, body){
+  }, function(err, res, body) {
     if (err) error(err);
     console.log(body);
   });
 }
 
-function packListChecker(packList, packListJson){
+function packListChecker(packList, packListJson) {
   // compare last pack ID and NAME
   // WHAT IF ORDER CHANGES BUT NOT LAST FILE
   // compare last 2-5 pack ids, size, and names?
 
-  fs.readFile(packListJson, 'utf8', function(err, data){
+  fs.readFile(packListJson, 'utf8', function(err, data) {
     // skip reading from json packlist since it doesn't exist
     if (err) error(err);
 
@@ -88,16 +88,16 @@ function packListChecker(packList, packListJson){
     var jsonPacklistLength = jsonPacklist.length;
 
     // get last 3 packs from packlist file
-    fs.readFile(packList, 'utf8', function(err, data){
+    fs.readFile(packList, 'utf8', function(err, data) {
       var counter = 3;
       var lastPacks = [];
       data = data.split('\n');
-      for (var i = data.length - 1; i >= 0; i--){
+      for (var i = data.length - 1; i >= 0; i--) {
         if (counter <= 0) {
           break;
         }
         var line = data[i].match(regexPack);
-        if (line){
+        if (line) {
           counter--;
           lastPacks.push({id: line[1], size: line[3], name: line[4]});
         }
@@ -105,9 +105,9 @@ function packListChecker(packList, packListJson){
       // reverse lastPacks array
       lastPacks.reverse();
       // compare last 3 packs to what json has
-      for (var i = lastPacks.length - 1; i >= 0; i--){
+      for (var i = lastPacks.length - 1; i >= 0; i--) {
         counter++;
-        if (lastPacks[i].id != jsonPacklist[jsonPacklistLength - counter].id || lastPacks[i].size != jsonPacklist[jsonPacklistLength - counter].size || lastPacks[i].name != jsonPacklist[jsonPacklistLength - counter].name){
+        if (lastPacks[i].id != jsonPacklist[jsonPacklistLength - counter].id || lastPacks[i].size != jsonPacklist[jsonPacklistLength - counter].size || lastPacks[i].name != jsonPacklist[jsonPacklistLength - counter].name) {
           // pack difference, update packlist
           var json = parsePacklist(packList);
           // savePacklistJson(json, packListJson);
